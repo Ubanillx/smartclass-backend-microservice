0025e34f58bbe3e61a575d85ed3579bdf7a58762
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:06:34 +0800

    feat(dailyarticle): 添加用户文章阅读记录的 Mapper 和 Service 接口
    - 新增 UserArticleRecordMapper 接口，用于用户文章阅读记录的数据库操作
    - 新增 UserArticleRecordService 接口，用于用户文章阅读记录的业务逻辑处理

---
+++ smartclass-backend-dailyarticle/src/main/java/com/ubanillx/smartclassbackenddailyarticle/mapper/UserArticleRecordMapper.java
@@ -0,0 +1,19 @@
+package com.ubanillx.smartclassbackenddailyarticle.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.dto.UserArticleRecord;
+
+
+/**
+* @author liulo
+* @description 针对表【user_article_record(用户文章阅读记录)】的数据库操作Mapper
+* @createDate 2025-03-20 14:36:38
+* @Entity com.ubanillx.smartclassbackendmodel.model.dto.UserArticleRecord
+*/
+public interface UserArticleRecordMapper extends BaseMapper<UserArticleRecord> {
+
+}
+
+
+
+
