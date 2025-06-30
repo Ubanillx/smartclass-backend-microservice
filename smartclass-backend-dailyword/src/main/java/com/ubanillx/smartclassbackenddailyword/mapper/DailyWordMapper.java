6ea5bee93359e9360ebadaceb7a8b4a586bcdc63
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:00:12 +0800

    feat(smartclass-backend-dailyword): 添加 DailyWordMapper 接口
    - 新增 DailyWordMapper接口，用于每日单词的数据库操作
    - 继承 BaseMapper，实现基本的 CRUD 功能- 位于 smartclass-backend-dailyword 模块中

---
+++ smartclass-backend-dailyword/src/main/java/com/ubanillx/smartclassbackenddailyword/mapper/DailyWordMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackenddailyword.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.DailyWord;
+
+/**
+* @author liulo
+* @description 针对表【daily_word(每日单词)】的数据库操作Mapper
+* @createDate 2025-03-19 00:03:09
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.DailyWord
+*/
+public interface DailyWordMapper extends BaseMapper<DailyWord> {
+
+}
+
+
+
+
