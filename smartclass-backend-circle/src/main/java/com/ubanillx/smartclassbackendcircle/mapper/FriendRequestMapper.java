fa4d16760e9bb84e05119dc05892593df0a8f165
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:07:29 +0800

    feat(friend-request): 添加好友申请功能
    - 实现了发送、 添加了查询好友申请的接口，接受、拒绝好友申请的功能
    -包括分页查询和按条件筛选
    - 实现了获取用户收到和发送的好友申请列表
    - 添加了删除好友申请的功能
    - 实现了获取用户收到的待处理好友申请数量

---
+++ smartclass-backend-circle/src/main/java/com/ubanillx/smartclassbackendcircle/mapper/FriendRequestMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendcircle.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.FriendRequest;
+
+/**
+* @author rog
+* @description 针对表【friend_request(好友申请表)】的数据库操作Mapper
+* @createDate 2025-05-04 15:37:31
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.FriendRequest
+*/
+public interface FriendRequestMapper extends BaseMapper<FriendRequest> {
+
+}
+
+
+
+
