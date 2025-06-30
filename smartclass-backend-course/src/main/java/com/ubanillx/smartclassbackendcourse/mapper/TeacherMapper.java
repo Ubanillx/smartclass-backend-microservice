1c6f56af1c608cb468ee8607f467e035feef469b
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:06:20 +0800

    feat(course): 添加讲师相关接口和服务
    - 新增 TeacherController 提供讲师相关接口
    - 新增 TeacherMapper 定义讲师数据库操作- 新增 TeacherService 接口和 TeacherServiceImpl 实现类，提供讲师服务
    - 实现讲师的增删改查功能
    - 添加获取讲师课程列表和推荐讲师列表的功能

---
+++ smartclass-backend-course/src/main/java/com/ubanillx/smartclassbackendcourse/mapper/TeacherMapper.java
@@ -0,0 +1,15 @@
+package com.ubanillx.smartclassbackendcourse.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.Teacher;
+
+/**
+ * 讲师数据库操作
+ */
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
+
+
+
+
