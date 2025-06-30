9d146f08c7477d5c8b672b8a5a303f08a27930a9
Ubanillx <liulongxin21@gmail.com>
2025-06-30 21:55:06 +0800

    feat(course): 添加课程评论功能- 新增 CourseReviewController、CourseReviewMapper、CourseReviewService 及其实现类 - 实现了课程评论的添加、删除、更新状态、回复、点赞等功能 - 提供了课程评论的分页查询和评分统计接口

---
+++ smartclass-backend-course/src/main/java/com/ubanillx/smartclassbackendcourse/mapper/CourseReviewMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendcourse.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.CourseReview;
+
+/**
+* @author liulo
+* @description 针对表【course_review(课程评价)】的数据库操作Mapper
+* @createDate 2025-03-21 15:14:50
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.CourseReview
+*/
+public interface CourseReviewMapper extends BaseMapper<CourseReview> {
+
+}
+
+
+
+
