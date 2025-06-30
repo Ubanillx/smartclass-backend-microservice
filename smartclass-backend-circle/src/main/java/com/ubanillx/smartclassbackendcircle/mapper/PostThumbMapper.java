a43e47121d15191b9fda2baf3d40e691cfc394e9
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:10:50 +0800

    feat(smartclass-backend-circle): 添加帖子点赞功能
    - 新增 PostThumbMapper 接口，用于帖子点赞的数据库操作
    - 新增帖子点赞的相关服务方法
    - 实现 PostThumbService 接口， PostThumbService 接口，定义提供帖子点赞的业务逻辑
    - 添加帖子点赞、取消点赞和判断是否已点赞等功能

---
+++ smartclass-backend-circle/src/main/java/com/ubanillx/smartclassbackendcircle/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.ubanillx.smartclassbackendcircle.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.PostThumb;
+
+/**
+ * 帖子点赞数据库操作
+*/
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
