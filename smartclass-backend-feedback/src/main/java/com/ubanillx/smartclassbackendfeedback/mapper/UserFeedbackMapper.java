ec4397bbd9d0c621869d447a83d6d86f91e94aac
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:03:34 +0800

    feat(feedback): 添加用户反馈功能模块
    - 实现了用户反馈的添加、删除、更新和查询功能
    - 添加了管理员处理用户反馈和回复的功能
    - 实现了用户未读回复数量的统计
    - 新增了UserFeedbackMapper、UserFeedbackService接口及其实现类

---
+++ smartclass-backend-feedback/src/main/java/com/ubanillx/smartclassbackendfeedback/mapper/UserFeedbackMapper.java
@@ -0,0 +1,19 @@
+package com.ubanillx.smartclassbackendfeedback.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.UserFeedback;
+
+
+/**
+* @author rog
+* @description 针对表【user_feedback(用户反馈)】的数据库操作Mapper
+* @createDate 2025-05-02 14:35:52
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.UserFeedback
+*/
+public interface UserFeedbackMapper extends BaseMapper<UserFeedback> {
+
+}
+
+
+
+
