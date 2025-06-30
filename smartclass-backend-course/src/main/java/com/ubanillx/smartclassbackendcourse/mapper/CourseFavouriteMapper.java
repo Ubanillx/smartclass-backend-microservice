5b917aee015a9e8ea7658fd29bd767946333ceef
Ubanillx <liulongxin21@gmail.com>
2025-06-30 21:54:20 +0800

    feat(course): 添加课程收藏功能- 新增 CourseFavouriteController 提供课程收藏相关接口 - 新增 CourseFavouriteMapper 和 CourseFavouriteService 进行数据库操作- 实现 CourseFavouriteServiceImpl完成课程收藏业务逻辑 - 添加收藏课程、取消收藏、检查是否收藏、获取收藏列表和收藏数量等功能

---
+++ smartclass-backend-course/src/main/java/com/ubanillx/smartclassbackendcourse/mapper/CourseFavouriteMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendcourse.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.CourseFavourite;
+
+/**
+* @author liulo
+* @description 针对表【course_favourite(课程收藏)】的数据库操作Mapper
+* @createDate 2025-03-21 15:14:50
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.CourseFavourite
+*/
+public interface CourseFavouriteMapper extends BaseMapper<CourseFavourite> {
+
+}
+
+
+
+
