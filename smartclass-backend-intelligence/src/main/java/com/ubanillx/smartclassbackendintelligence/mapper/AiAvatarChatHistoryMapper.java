be2e5f3198ecb2ef4bd6773660ad2fabdf90f009
Ubanillx <liulongxin21@gmail.com>
2025-06-30 21:46:11 +0800

    feat(smartclass-backend-intelligence): 添加 AI 分身对话历史数据库操作 Mapper
    - 新增 AiAvatarChatHistoryMapper 接口，用于 AI 分身对话历史表的数据库操作
    - 该 Mapper 继承自 BaseMapper，提供了基本的 CRUD 功能
    - 此改动为智能课堂后端增加了对 AI 分身对话历史数据的持久化支持

---
+++ smartclass-backend-intelligence/src/main/java/com/ubanillx/smartclassbackendintelligence/mapper/AiAvatarChatHistoryMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendintelligence.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.AiAvatarChatHistory;
+
+/**
+* @author liulo
+* @description 针对表【ai_avatar_chat_history(AI分身对话历史)】的数据库操作Mapper
+* @createDate 2025-03-24 21:35:44
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.AiAvatarChatHistory
+*/
+public interface AiAvatarChatHistoryMapper extends BaseMapper<AiAvatarChatHistory> {
+
+}
+
+
+
+
