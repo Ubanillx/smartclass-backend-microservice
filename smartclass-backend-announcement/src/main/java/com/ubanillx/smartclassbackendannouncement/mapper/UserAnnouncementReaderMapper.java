8d92939865ae8fe66f7640b37f1fb91617a3e308
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:13:40 +0800

    feat(backend-announcement): 添加用户公告阅读记录 Mapper
    - 新增 UserAnnouncementReaderMapper 接口
    - 实现用户公告阅读记录的基本数据操作

---
+++ smartclass-backend-announcement/src/main/java/com/ubanillx/smartclassbackendannouncement/mapper/UserAnnouncementReaderMapper.java
@@ -0,0 +1,14 @@
+package com.ubanillx.smartclassbackendannouncement.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.UserAnnouncementReader;
+
+/**
+ * 用户公告阅读记录Mapper
+ */
+public interface UserAnnouncementReaderMapper extends BaseMapper<UserAnnouncementReader> {
+}
+
+
+
+
