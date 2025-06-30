a463019000f18da2c64c20bce419891e386f0555
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:09:05 +0800

    feat(smartclass-backend-circle): 添加帖子评论相关功能
    - 新增 PostCommentMapper 接口，用于帖子评论的数据库操作
    - 实现 PostCommentService 接口，提供帖子评论的业务逻辑
    - 编写 PostCommentServiceImpl 类，实现帖子评论的具体业务方法
    - 功能包括：验证评论、查询评论、添加评论、删除评论等

---
+++ smartclass-backend-circle/src/main/java/com/ubanillx/smartclassbackendcircle/mapper/PostCommentMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendcircle.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.PostComment;
+
+/**
+* @author rog
+* @description 针对表【post_comment(帖子评论)】的数据库操作Mapper
+* @createDate 2025-05-03 17:31:42
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.PostComment
+*/
+public interface PostCommentMapper extends BaseMapper<PostComment> {
+
+}
+
+
+
+
