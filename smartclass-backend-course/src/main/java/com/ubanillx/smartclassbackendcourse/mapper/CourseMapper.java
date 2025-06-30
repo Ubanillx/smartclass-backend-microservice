cacbe109e545ab3b687151e475a8e2b10602260b
Ubanillx <liulongxin21@gmail.com>
2025-06-30 21:54:32 +0800

    feat(smartclass-backend-course): 添加 CourseMapper 接口
    - 新增 CourseMapper 接口，用于课程表的数据库操作
    -继承 BaseMapper，提供基本的 CRUD 功能
    - 接口中暂无自定义方法

---
+++ smartclass-backend-course/src/main/java/com/ubanillx/smartclassbackendcourse/mapper/CourseMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendcourse.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.Course;
+
+/**
+* @author liulo
+* @description 针对表【course(课程)】的数据库操作Mapper
+* @createDate 2025-03-18 23:08:38
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.Course
+*/
+public interface CourseMapper extends BaseMapper<Course> {
+
+}
+
+
+
+
