4be30224b47cec76f374e2bdb9a31ac589a2004d
Ubanillx <liulongxin21@gmail.com>
2025-06-30 21:48:15 +0800

    feat(backend): 添加公告模块数据库操作接口
    - 新增 AnnouncementMapper 接口，用于系统公告表的数据库操作
    - 继承 BaseMapper，提供基本的 CRUD功能
    - 定义在 com.ubanillx.smartclassbackendannouncement.mapper 包中

---
+++ smartclass-backend-announcement/src/main/java/com/ubanillx/smartclassbackendannouncement/mapper/AnnouncementMapper.java
@@ -0,0 +1,19 @@
+package com.ubanillx.smartclassbackendannouncement.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.Announcement;
+
+
+/**
+* @author liulo
+* @description 针对表【announcement(系统公告)】的数据库操作Mapper
+* @createDate 2025-03-18 23:08:38
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.Announcement
+*/
+public interface AnnouncementMapper extends BaseMapper<Announcement> {
+
+}
+
+
+
+
