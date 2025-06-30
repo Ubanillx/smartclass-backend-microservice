ce5ba8f37426bac7a0fafc1faa75c456b22db589
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:09:31 +0800

    feat(smartclass-backend-circle): 添加好友关系功能
    - 新增 FriendRelationshipController 提供好友关系相关接口
    - 新增 FriendRelationshipMapper用于好友关系表的数据库操作- 新增 FriendRelationshipService 接口定义好友关系服务方法
    - 实现 FriendRelationshipServiceImpl 提供好友关系服务的具体实现

---
+++ smartclass-backend-circle/src/main/java/com/ubanillx/smartclassbackendcircle/mapper/FriendRelationshipMapper.java
@@ -0,0 +1,18 @@
+package com.ubanillx.smartclassbackendcircle.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.FriendRelationship;
+
+/**
+* @author rog
+* @description 针对表【friend_relationship(好友关系表)】的数据库操作Mapper
+* @createDate 2025-05-04 15:37:31
+* @Entity com.ubanillx.smartclassbackendmodel.model.entity.FriendRelationship
+*/
+public interface FriendRelationshipMapper extends BaseMapper<FriendRelationship> {
+
+}
+
+
+
+
