9c4a63753b1244df5066e98e77daacf0e4253068
Ubanillx <liulongxin21@gmail.com>
2025-06-30 22:11:22 +0800

    feat(smartclass-backend-circle): 添加私聊消息功能
    - 新增 PrivateMessageMapper、PrivateMessageService 和 PrivateMessageServiceImpl 类
    - 实现私聊消息发送、接收、查询 添加用户权限验证和消息推送机制和标记为已读等功能
    -

---
+++ smartclass-backend-circle/src/main/java/com/ubanillx/smartclassbackendcircle/mapper/PrivateMessageMapper.java
@@ -0,0 +1,14 @@
+package com.ubanillx.smartclassbackendcircle.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ubanillx.smartclassbackendmodel.model.entity.PrivateMessage;
+
+/**
+ * 私聊消息Mapper
+ */
+public interface PrivateMessageMapper extends BaseMapper<PrivateMessage> {
+}
+
+
+
+
