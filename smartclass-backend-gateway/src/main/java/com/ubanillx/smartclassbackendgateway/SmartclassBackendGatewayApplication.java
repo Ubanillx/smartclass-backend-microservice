bdb99f3e75ab4852f1731b78fc5e31efe791de32
Ubanillx <liulongxin21@gmail.com>
2025-07-01 13:02:57 +0800

    feat(smartclass-backend gateway):启用服务发现功能
    - 在 SmartclassBackendGatewayApplication 类中添加 @EnableDiscoveryClient 注解
    - 此修改使应用程序能够作为 Eureka 客户端参与服务发现

--- smartclass-backend-gateway/src/main/java/com/ubanillx/smartclassbackendgateway/SmartclassBackendGatewayApplication.java
+++ smartclass-backend-gateway/src/main/java/com/ubanillx/smartclassbackendgateway/SmartclassBackendGatewayApplication.java
@@ -2,8 +2,10 @@ package com.ubanillx.smartclassbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SmartclassBackendGatewayApplication {
 
     public static void main(String[] args) {
